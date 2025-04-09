cafe7c4eb6b713bb180f719e00cd70435a5ffffd
GH Cao <driver1998.ms@outlook.com>
2025-04-09 23:47:19 +0800

    initial commit

---
+++ CloudGenshin/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.ViewManagement;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.Core;
+
+namespace CloudGenshin;
+
+public sealed partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs e)
+    {
+        ApplicationView.GetForCurrentView().TitleBar.BackgroundColor = Colors.Transparent;
+        ApplicationView.GetForCurrentView().TitleBar.ButtonBackgroundColor = Colors.Transparent;
+        ApplicationView.GetForCurrentView().TitleBar.InactiveBackgroundColor = Colors.Transparent;
+        ApplicationView.GetForCurrentView().TitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+        CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+
+        Window.Current.Content = new MainPage();
+        Window.Current.Activate();
+    }
+}
---
+++ CloudGenshin/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Windows.ApplicationModel.Core;
+using Windows.System;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace CloudGenshin;
+
+public sealed partial class MainPage : Page
+{
+    private string desktopUserAgent = "";
+    private string mobileUserAgent = "";
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+
+        Window.Current.SetTitleBar(TitleBarRegion);
+
+        var dispatcherQueue = DispatcherQueue.GetForCurrentThread();
+        WebView.EnsureCoreWebView2Async().AsTask().ContinueWith(task =>
+        {
+            dispatcherQueue.TryEnqueue(() =>
+            {
+                desktopUserAgent = (WebView.CoreWebView2.Settings.UserAgent);
+                mobileUserAgent = desktopUserAgent.Replace("Windows NT 10.0;", "Linux; Android 12.0;").Replace("Safari/", "Mobile Safari/");
+            });
+        });
+
+        ApplicationView.GetForCurrentView().VisibleBoundsChanged += (s, e) =>
+        {
+            if (s.IsFullScreenMode)
+            {
+                CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = false;
+                TitleBarRegion.Visibility = Visibility.Collapsed;
+                CommandPanel.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+                TitleBarRegion.Visibility = Visibility.Visible;
+                CommandPanel.Visibility = Visibility.Visible;
+            }
+        };
+
+
+        WebView.Focus(FocusState.Programmatic);
+    }
+
+    private void FullScreen_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+    {
+        var view = ApplicationView.GetForCurrentView();
+        if (view.IsFullScreenMode)
+        {
+            view.ExitFullScreenMode();
+        }
+        else
+        {
+            view.TryEnterFullScreenMode();
+        }
+    }
+
+    private void TouchModeToggle_Checked(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+    {
+        WebView.CoreWebView2.Settings.UserAgent = mobileUserAgent;
+        WebView.CoreWebView2.Reload();
+    }
+
+    private void TouchModeToggle_Unchecked(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+    {
+        WebView.CoreWebView2.Settings.UserAgent = desktopUserAgent;
+        WebView.CoreWebView2.Reload();
+    }
+}
